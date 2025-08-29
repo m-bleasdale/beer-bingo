6272d74ee05d2a027e780cbbd7824d33cc9a0763
LordMorgana <morganbleasdale234@gmail.com>
2025-08-29 16:00:57 +0100

    feedback responsiveness 2

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -18,14 +18,14 @@ export default function Page () {
 
             <h2 className="text-3xl font-medium">Feedback</h2>
 
-            <div className="flex flex-col w-full px-20 lg:w-200 gap-10 items-center">
+            <div className="flex flex-col w-full lg:w-200 gap-10 items-center">
                 <p className="text-md text-center">This project is designed to make tracking how much alcohol you're consuming fun. By tracking alcohol intake, people become more aware of how much they are drinking, and if they need to consider cutting down.</p>
                 <p className="text-md text-center">If you have feedback on how to improve this app, please use the form linked below.</p>
                 <a href="https://forms.gle/jbs9k1SFkK113Gwu5">
                     <Button>Submit Feedback</Button>
                 </a>
 
-                <div className="flex flex-col w-full px-20 lg:w-200 gap-4">
+                <div className="flex flex-col w-full lg:w-200 gap-4">
                     <h2 className="text-lg font-semibold">v1.0.1 Changelog, 29th August 2025</h2>
                     <p className="text-md">Based on the last round of community feedback, the following has been updated:</p>
                     <p className="text-md">- Added support for non-beer drinks, such as mixers and shots</p>
